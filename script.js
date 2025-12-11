5f7fb38126dc2ca0fafcd4b041e8bc81c93ca133
AlexDang <tuantunhht@gmail.com>
2025-12-11 08:45:06 +0700

    1st commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt('Enter the width');
+inputHeight = prompt('Enter the height');
+
+let area = inputWidth * inputHeight;
+document.write('The area is ' + area);
