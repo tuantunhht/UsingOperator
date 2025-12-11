abcbad31c6874187bf609d4b6a20c6186eccdf3a
AlexDang <tuantunhht@gmail.com>
2025-12-11 08:50:26 +0700

    update script

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ inputWidth = prompt('Enter the width');
 inputHeight = prompt('Enter the height');
 let Width = parseInt(inputWidth);
 let Height = parseInt(inputHeight);
-let area = Width * hitHeight;
+let area = Width * Height;
 document.write('The area is ' + area);
